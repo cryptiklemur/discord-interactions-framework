4830e2ed1b232c60460878a2a6f74406e1efeef1
Aaron Scherer <aequasi@gmail.com>
2020-12-19 12:35:40 -0600

    Release 0.1.2

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {Command} from './Command';
+export {Config} from './Config';
+export {Registry} from './Registry';
+export {AbstractProcessor} from './Processor/AbstractProcessor';
+export {CloudflareWorkerProcessor} from './Processor/CloudflareWorkerProcessor';
+export {ExpressProcessor} from './Processor/ExpressProcessor';
+export type {ProcessorInterface} from './Processor/ProcessorInterface';
+export * as types from './types';
