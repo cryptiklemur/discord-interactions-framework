2151a73ed1462cfcbe2f3a321304d3d95a5314f6
Aaron Scherer <aequasi@gmail.com>
2020-12-19 19:36:15 -0600

    Release 0.2.0

--- src/types.ts
+++ src/types.ts
@@ -32,16 +32,59 @@ export interface InteractionResponse {
 
 export interface InteractionApplicationCommandCallbackData {
     readonly tts?: boolean;
-    readonly content: string;
-    readonly embeds: Embed[];
+    readonly content?: string;
+    readonly embeds?: Embed[];
     readonly flags?: InteractionResponseFlags | number;
-    readonly allowedMentions?: AllowedMentions;
+    readonly allowed_mentions?: AllowedMentions;
 }
 
 export interface Embed {
+    title?: string;
+    type?: 'rich';
+    description?: string;
+    url?: string;
+    timestamp?: string;
+    color?: number;
+    footer?: {
+        text: string;
+        icon_url?: string;
+    };
+    image?: {
+        url?: string;
+        height?: number;
+        width?: number;
+    };
+    thumbnail?: {
+        url?: string;
+        height?: number;
+        width?: number;
+    };
+    video?: {
+        url?: string;
+        height?: number;
+        width?: number;
+    }
+    provider?: {
+        name?: string;
+        url?: string;
+    };
+    author?: {
+        name?: string;
+        url?: string;
+        icon_url?: string;
+    };
+    fields?: {
+        name: string;
+        value: string;
+        inline?: boolean;
+    }[];
 }
 
 export interface AllowedMentions {
+    parse: ('roles'|'users'|'everyone')[];
+    roles?: Snowflake[];
+    users?: Snowflake[];
+    replied_user?: boolean;
 }
 
 export type InteractionHandler<N extends string, T extends InteractionType, O extends ApplicationCommandOptions> = (interaction: Interaction<N, T, O>) => InteractionResponse;
